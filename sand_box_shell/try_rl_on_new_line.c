fe7b25e165ad90ff568c328d34181507ad1fbeed
Ahmed Salem <ahsalem@lab3r3s12.42abudhabi.ae>
2022-09-14 07:27:09 +0400

    readline with example

---
+++ sand_box_shell/try_rl_on_new_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *a ;
+
+	while (1)
+	{
+	//prompt the user for input with the message below
+	a = readline("minishel tester $> ");
+	//saved it to a
+	//then print it back to the user 
+	printf("%s\n", a);
+	printf("\n%d\n", rl_on_new_line());
+	free(a);
+	}
+}
